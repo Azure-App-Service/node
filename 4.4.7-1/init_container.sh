8151c4e02ffacbe6a0731fb44b7477da7579aed2
naziml <naziml@gmail.com>
2017-03-01 18:21:49 -0800

    Start ssh server in init

--- 4.4.7-1/init_container.sh
+++ 4.4.7-1/init_container.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+service ssh start
 touch /home/LogFiles/node_$WEBSITE_ROLE_INSTANCE_ID_out.log
 echo "$(date) Container started" >> /home/LogFiles/node_$WEBSITE_ROLE_INSTANCE_ID_out.log
