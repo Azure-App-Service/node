d44b228bd260f17845c0e7a5a5f323492b5c2f6f
Nick Walker <nickwalk@microsoft.com>
2017-06-15 13:57:32 -0700

    Build.sh correction

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x -e
 
-docker build -t "$1"/node:4.5.0-2 -t "$1"/node:4.5-2 -t "$1"/node:4-2 4.5
+docker build -t "$1"/node:4.5.0-2 -t "$1"/node:4.5-2 -t 4.5
 docker build -t "$1"/node:4.8.3 -t "$1"/node:4.8 -t "$1"/node:4 4.8
 docker build -t "$1"/node:6.11.0 -t "$1"/node:6.11 -t "$1"/node:6 -t "$1"/node:lts 6.11
 docker build -t "$1"/node:8.0.0 -t "$1"/node:8.0 8.0.0
@@ -11,7 +11,6 @@ docker login -u "$2" -p "$3"
 
 docker push "$1"/node:4.5.0-2
 docker push "$1"/node:4.5-2
-docker push "$1"/node:4-2
 
 docker push "$1"/node:4.8.3
 docker push "$1"/node:4.8
