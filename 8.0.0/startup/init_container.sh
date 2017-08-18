217d21e93921a612ea74aa7a7650f4d7eb49368f
naziml <naziml@gmail.com>
2017-08-17 23:30:08 -0700

    Remove node log file

--- 8.0.0/startup/init_container.sh
+++ 8.0.0/startup/init_container.sh
@@ -5,9 +5,6 @@ mkdir "$PM2HOME"
 chmod 777 "$PM2HOME"
 ln -s /home/LogFiles "$PM2HOME"/logs
 
-touch /home/LogFiles/node_${WEBSITE_ROLE_INSTANCE_ID}_out.log
-echo "$(date) Container started" >> /home/LogFiles/node_${WEBSITE_ROLE_INSTANCE_ID}_out.log
-
 echo "$@" > /opt/startup/startupCommand
 node /opt/startup/generateStartupCommand.js
 
