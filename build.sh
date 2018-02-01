9f26ab7b377d31850d52b365017c4ef9993e6adb
Lukasz Stempniewicz <lukaszs@microsoft.com>
2018-02-01 08:44:14 -0800

    Update build.sh to push :latest

--- build.sh
+++ build.sh
@@ -23,6 +23,7 @@ docker build -t "$1"/node:8.8.0_"$buildnumber" 8.8.0
 docker build -t "$1"/node:8.8.1_"$buildnumber" 8.8.1
 docker build -t "$1"/node:8.9.4_"$buildnumber" -t "$1"/node:lts_"$buildnumber" 8.9.4
 docker build -t "$1"/node:9.4.0_"$buildnumber" -t "$1"/node:latest_"$buildnumber" 9.4.0
+docker tag "$1"/node:latest_"$buildnumber" "$1"/node:latest 
 
 docker login -u "$2" -p "$3"
 
@@ -48,5 +49,6 @@ docker push "$1"/node:8.9.4_"$buildnumber"
 docker push "$1"/node:lts_"$buildnumber"
 docker push "$1"/node:9.4.0_"$buildnumber"
 docker push "$1"/node:latest_"$buildnumber"
+docker push "$1"/node:latest
 
 docker logout
