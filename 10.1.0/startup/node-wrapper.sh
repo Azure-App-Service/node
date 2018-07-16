67fd270714913533180762760c78510e72c995a7
Patrick Lee <31744877+patricklee2@users.noreply.github.com>
2018-07-16 14:08:44 -0700

    fixes bug where *.json *.js could not be used as startup files

--- 10.1.0/startup/node-wrapper.sh
+++ 10.1.0/startup/node-wrapper.sh
@@ -21,6 +21,12 @@ then
   package_json=`cat /opt/startup/PACKAGE_JSON_FLAG`
 fi
 
+process_json="FALSE"
+if [ -e "/opt/startup/PROCESS_JSON_FLAG" ]
+then
+  process_json=`cat /opt/startup/PROCESS_JSON_FLAG`
+fi
+
 custom_startup="FALSE"
 if [ -e "/opt/startup/CUSTOM_STARTUP_CMD_FLAG" ]
 then
@@ -28,11 +34,21 @@ then
 fi
 
 # enable remote debugging when node started with npm
-if [ "$APPSVC_REMOTE_DEBUGGING" = "TRUE" ] && ([ "$package_json" = "TRUE" ] || [ "$custom_startup" = "TRUE" ]) && [ $args_contain_inspect = false ]
+if [ "$APPSVC_REMOTE_DEBUGGING" = "TRUE" ] && [ "$package_json" = "TRUE" ] && [ $args_contain_inspect = false ]
 then
   node-original --inspect=0.0.0.0:$APPSVC_TUNNEL_PORT "$@"
-elif [ "$APPSVC_REMOTE_DEBUGGING_BREAK" = "TRUE" ] && ([ "$package_json" = "TRUE" ] || [ "$custom_startup" = "TRUE" ]) && [ $args_contain_inspect = false ]
+elif [ "$APPSVC_REMOTE_DEBUGGING_BREAK" = "TRUE" ] && [ "$package_json" = "TRUE" ] && [ $args_contain_inspect = false ]
 then 
+  node-original --inspect-brk=0.0.0.0:$APPSVC_TUNNEL_PORT "$@"
+# if using pm2
+elif ([ "$APPSVC_REMOTE_DEBUGGING" = "TRUE" ] || [ "$APPSVC_REMOTE_DEBUGGING_BREAK" = "TRUE" ] ) && [ "$process_json" = "TRUE" ] && [ $args_contain_inspect = false ]
+then
+  node-original "$@"
+elif [ "$APPSVC_REMOTE_DEBUGGING" = "TRUE" ] && [ "$custom_startup" = "TRUE" ] && [ $args_contain_inspect = false ]
+then
+  node-original --inspect=0.0.0.0:$APPSVC_TUNNEL_PORT "$@"
+elif [ "$APPSVC_REMOTE_DEBUGGING_BREAK" = "TRUE" ] && [ "$custom_startup" = "TRUE" ] && [ $args_contain_inspect = false ]
+then
   node-original --inspect-brk=0.0.0.0:$APPSVC_TUNNEL_PORT "$@"
 else
   node-original "$@"
