e3982d5336b7531d2d6449e79702b672c59e0b6c
Patrick Lee <31744877+patricklee2@users.noreply.github.com>
2019-02-07 16:04:08 -0800

    sync alpine images

--- 10.14.1/startup/init_container.sh
+++ 10.14.1/startup/init_container.sh
@@ -25,8 +25,25 @@ eval $(printenv | awk -F= '{print "export " $1"="$2 }' >> /etc/profile)
 # starting sshd process
 /usr/sbin/sshd
 
+# feature flag for remote debugging for with npm
+# set flag and restart site to remove these changes
+if [ "$APPSVC_REMOTE_DEBUGGING" = "TRUE" ] && [ ! "$APPSETTING_REMOTE_DEBUGGING_FEATURE_FLAG" = "FALSE" ]
+then
+        mv /usr/local/bin/node /usr/local/bin/node-original
+        mv /opt/startup/node-wrapper.sh /usr/local/bin/node
+        chmod a+x /usr/local/bin/node
+        sed -i 's/env node/env node-original/' /usr/local/lib/node_modules/npm/bin/npm-cli.js
+        sed -i 's/env node/env node-original/' /usr/local/lib/node_modules/npm/bin/npx-cli.js
+        sed -i 's/env node/env node-original/' /usr/local/lib/node_modules/pm2/bin/pm2
+        sed -i 's/env node/env node-original/' /usr/local/lib/node_modules/pm2/bin/pm2-dev
+        sed -i 's/env node/env node-original/' /usr/local/lib/node_modules/pm2/bin/pm2-docker
+        sed -i 's/env node/env node-original/' /usr/local/lib/node_modules/pm2/bin/pm2-runtime
+        sed -i 's/env node/env node-original/' /opt/startup/generateStartupCommand.js
+fi
+
 echo "$@" > /opt/startup/startupCommand
 node /opt/startup/generateStartupCommand.js
+chmod 755 /opt/startup/startupCommand
 
 STARTUPCOMMAND=$(cat /opt/startup/startupCommand)
 echo "Running $STARTUPCOMMAND"
